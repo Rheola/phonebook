8f3474f0d594163f36fbd856afe3bdf68b91f72c
Svetlana Okuneva <rheola@yandex.ru>
2020-06-28 17:23:24 +0300

    index.php 404.php

---
+++ views/404.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Ой!</h1>
+                <h2>Ошибка 404
+                    </h2>
+                <div class="error-details">
+                    Страница не найдена
+                </div>
+                <div class="error-actions">
+                    <a href="/" class="btn btn-primary btn-lg">
+                        <span class="glyphicon glyphicon-home"></span>
+                        На главную
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
