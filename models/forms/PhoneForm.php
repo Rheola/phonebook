c3ec07a3d6e2bf6c75a4c91517d834e73e5535ea
Svetlana Okuneva <rheola@yandex.ru>
2020-06-30 12:20:12 +0300

    PhoneForm

--- models/forms/PhoneForm.php
+++ models/forms/PhoneForm.php
@@ -4,7 +4,9 @@
 namespace models\forms;
 
 
+use core\App;
 use core\forms\Form;
+use models\Phone;
 
 class PhoneForm extends Form
 {
@@ -20,16 +22,86 @@ class PhoneForm extends Form
         if (!empty($this->email)) {
             if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
                 $this->errors['email'][] = 'Неверный формат email';
-
-                return false;
             }
         }
 
         if (empty($this->phone)) {
             $this->errors['phone'][] = 'Укажите телефон';
+        }
+
+
+        if (!empty($_FILES['PhoneForm']['tmp_name']['file'])) {
+            $this->validateFile();
+        }
+
+        return !$this->hasErrors();
+    }
+
+    public function validateFile()
+    {
+        $size = $_FILES['PhoneForm']['size']['file'];
+
+
+        if ($size > 2 * 1024 * 1024) {
+            $this->errors['file'][] = 'Слишком большой файл. Максимальный размер - 2 МБ';
+
+            return false;
+        }
+
+        $fileType = pathinfo($_FILES['PhoneForm']['name']['file'], PATHINFO_EXTENSION);
+        $fileType = strtolower($fileType);
+        $allowedTypes = ['jpg', 'png'];
+
+        if (!in_array($fileType, $allowedTypes)) {
+            $this->errors['file'][] = 'Разрешенные типы файлов jpg и png';
+
             return false;
         }
 
         return true;
+
+
     }
+
+    public function uploadFile()
+    {
+        $ext = pathinfo($_FILES['PhoneForm']['name']['file'], PATHINFO_EXTENSION);
+
+        while (true) {
+
+            $unique = md5(uniqid(mt_rand(), true));
+            $fileName = $unique . '.' . $ext;
+            $filePath = App::getInstance()->getRootPath() . '/upload/' . $fileName;
+            if (!file_exists($filePath)) {
+                break;
+            }
+        }
+        if (move_uploaded_file($_FILES['PhoneForm']['tmp_name']['file'], $filePath)) {
+            return $fileName;
+        }
+
+        return false;
+    }
+
+
+    public function savePhone(Phone $phone)
+    {
+        $phone->user_id = App::$user->id;
+        preg_match_all('/\d+/', $this->phone, $matches);
+        $phone->phone = implode('', $matches[0]);
+        $phone->email = $this->email;
+        $phone->first_name = $this->first_name;
+        $phone->last_name = $this->last_name;
+
+        if (!empty($_FILES['PhoneForm']['tmp_name']['file'])) {
+            $fileName = $this->uploadFile();
+            $phone->file = $fileName;
+            $phone->makeThumbnails(100, 100);
+        }
+        $phone->save();
+
+        return $phone;
+    }
+
+
 }
