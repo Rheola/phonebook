c09a5580c49ac7a1cef77b1bc78362d30b370392
Svetlana Okuneva <rheola@yandex.ru>
2020-06-30 12:37:11 +0300

    fontawesome

--- views/main.php
+++ views/main.php
@@ -20,7 +20,7 @@ use core\App;
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.css" rel="stylesheet">
-
+    <script src="https://kit.fontawesome.com/a9adfc370a.js" crossorigin="anonymous"></script>
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -39,6 +39,12 @@ use core\App;
     </style>
     <!-- Custom styles for this template -->
     <link href="/css/main.css" rel="stylesheet">
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery.slim.min.js"><\/script>')</script>
 </head>
 <body>
 
@@ -100,10 +106,7 @@ use core\App;
         <?php include __DIR__ . '/' . $template . '.php'; ?>
     </main>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="/js/vendor/jquery.slim.min.js"><\/script>')</script>
+
 <script src="/js/bootstrap.bundle.js"></script>
 </body>
 </html>
