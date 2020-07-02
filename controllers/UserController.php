a4bd44da95943017d076d1e7c073652e0547555d
okuneva.s <rheola@yandex.ru>
2020-07-02 15:46:48 +0300

    исправление сохранения пароля

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -27,7 +27,6 @@ class UserController extends Controller
         if (isset($_POST['LoginForm'])) {
             $form->load($_POST['LoginForm']);
 
-
             if ($form->validate() && $form->authenticate()) {
                 $this->redirect('/phone/');
             }
@@ -57,8 +56,8 @@ class UserController extends Controller
             if ($form->validate()) {
                 $user = new User();
                 $user->login = $form->login;
-                $user->email = $form->email;
-                $user->password = md5($user->password);
+                $user->email = mb_strtolower($form->email);
+                $user->password = $form->hashPassword();
 
                 $user->save();
 
