d16bd8ce046ac35fdf20037bc226120caf8fdd62
okuneva.s <rheola@yandex.ru>
2020-07-03 14:19:58 +0300

    Поправлена верстка форм

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -17,19 +17,23 @@
                 $loginForm->printErrors();
             }
             ?>
+            <div class="form-group">
+                <label for="inputEmail">Email</label>
+                <input name="LoginForm[email]" type="email" id="inputEmail" class="form-control"
+                       placeholder="Email address" required autofocus value="<?= $loginForm->email ?>">
+            </div>
 
-            <label for="inputEmail">Email</label>
-            <input name="LoginForm[email]" type="email" id="inputEmail" class="form-control"
-                   placeholder="Email address" required autofocus value="<?= $loginForm->email ?>">
+            <div class="form-group">
+                <label for="inputPassword">Пароль</label>
+                <input name="LoginForm[password]" type="password" id="inputPassword" class="form-control"
+                       placeholder="Пароль" required value="">
+            </div>
 
-
-            <label for="inputPassword">Пароль</label>
-            <input name="LoginForm[password]" type="password" id="inputPassword" class="form-control"
-                   placeholder="Пароль" required value="">
-
-            <label for="inputCaptcha">Капча </label>
-            <input name="LoginForm[captcha]" type="text" id="inputCaptcha"
-                   class="form-control captcha-input" placeholder="Код" required value="<?= $loginForm->captcha ?>">
+            <div class="form-group">
+                <label for="inputCaptcha">Капча </label>
+                <input name="LoginForm[captcha]" type="text" id="inputCaptcha"
+                       class="form-control captcha-input" placeholder="Код" required value="<?= $loginForm->captcha ?>">
+            </div>
 
             <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
         </form>
--- app/views/user/register.php
+++ app/views/user/register.php
@@ -16,30 +16,49 @@
                 $form->printErrors();
             }
             ?>
+            <div class="form-group">
+                <label for="inputEmail">Email </label>
+                <input name="RegisterForm[email]" type="email" id="inputEmail"
+                       class="form-control" placeholder="Email" required autofocus value="<?= $form->email ?>">
+
+            </div>
 
 
-            <label for="inputEmail">Email </label>
-            <input name="RegisterForm[email]" type="email" id="inputEmail"
-                   class="form-control" placeholder="Email" required autofocus value="<?= $form->email ?>">
+            <div class="form-group">
+                <label for="inputLogin">Логин</label>
+                <input name="RegisterForm[login]" type="text" id="inputLogin"
+                       class="form-control" placeholder="Логин" required value="<?= $form->login ?>">
 
-            <label for="inputLogin">Логин</label>
-            <input name="RegisterForm[login]" type="text" id="inputLogin"
-                   class="form-control" placeholder="Логин" required value="<?= $form->login ?>">
+            </div>
 
-            <label for="inputPassword">Пароль</label>
-            <input name="RegisterForm[password]" type="password" id="inputPassword"
-                   class="form-control" placeholder="Пароль" required value="<?= $form->password ?>">
+            <div class="form-group">
+                <label for="inputPassword">Пароль</label>
+                <input name="RegisterForm[password]" type="password" id="inputPassword"
+                       class="form-control" placeholder="Пароль" required value="<?= $form->password ?>">
 
-            <label for="inputPassword">Повтор пароля </label>
-            <input name="RegisterForm[passwordRepeat]" type="password" id="passwordRepeat"
-                   class="form-control" placeholder="Повтор пароля" required value="<?= $form->passwordRepeat ?>">
+            </div>
 
 
-            <label for="inputCaptcha">Капча </label>
-            <input name="RegisterForm[captcha]" type="text" id="inputCaptcha"
-                   class="form-control captcha-input" placeholder="Код" required value="<?= $form->captcha ?>">
+            <div class="form-group">
+                <label for="inputPassword">Повтор пароля </label>
+                <input name="RegisterForm[passwordRepeat]" type="password" id="passwordRepeat"
+                       class="form-control" placeholder="Повтор пароля" required value="<?= $form->passwordRepeat ?>">
+
+            </div>
+
+
+            <div class="form-group">
+                <label for="inputCaptcha">Капча </label>
+                <input name="RegisterForm[captcha]" type="text" id="inputCaptcha"
+                       class="form-control captcha-input" placeholder="Код" required value="<?= $form->captcha ?>">
+
+            </div>
+
 
             <button class="btn btn-lg btn-primary btn-block" type="submit">Зарегистрироваться</button>
+
+            <a href="/user/login">Войти</a>
+
         </form>
     </div>
 </div>
