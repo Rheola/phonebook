0f2c60ee59d496251b5d786d96704965915a5f1b
okuneva.s <rheola@yandex.ru>
2020-07-03 10:43:14 +0300

    readme.md

---
+++ params_template.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'salt' => '',
+    'db' =>
+        [
+            'host' => 'mysqldb',
+            'user' => '',
+            'password' => '',
+            'dbname' => '',
+        ],
+];
---
+++ readme.md
@@ -0,0 +1,7 @@
+How to start
+
+    Copy params_template.php to ../params.php
+    
+    Change .env variables
+    
+    Run migrations docker-compose up -d
